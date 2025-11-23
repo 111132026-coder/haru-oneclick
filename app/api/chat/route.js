e0acfa8f9c31e40ed56176884deafe438ecf88d6
111132026-coder <111132026@gas.fgu.edu.tw>
2025-11-23 17:40:39 +0800

    Delete app directory

--- app/api/chat/route.js
+++
@@ -1,7 +0,0 @@
-
-export async function POST(req) {
-  const { message } = await req.json();
-  return new Response(JSON.stringify({ reply: "Haru 收到：" + message }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Page() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Haru 小精靈</h1>
-      <p>網站已成功部署！未來可在此加入聊天介面。</p>
-    </main>
-  );
-}
